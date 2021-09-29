cc74886ed0ce6ef508ff39c9f9a68a8c863a7d30
Will Sanders <williamsanders81@gmail.com>
2021-09-29 09:59:18 -0700

    React Jokes Exercise

---
+++ react-jokes-classes/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import JokeList from "./JokeList";
+import JokeListCl from "./JokeList";
+function App() {
+  return (
+    <div className="App">
+      <JokeListCl />
+    </div>
+  );
+}
+
+export default App;
